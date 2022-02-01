35cdf97209f2fb1effdd52c64455a6699926df89
arid0s <48732432+arid0s@users.noreply.github.com>
2022-02-01 18:09:07 +0200

    Moon
    Add moon hover animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
-  imgPath = '../../assets/logohd.png';
-
+  main = '../../assets/logohd.png';
+  second = '../../assets/logohd_selected.png';
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -18,13 +19,4 @@ export class HeaderComponent implements OnInit {
   navigateToHome() {
     this.router.navigate(['/home']);
   }
-
-  onMouseEnter() {
-    //this.imgPath = '../../assets/logohd_selected.png';
-  }
-
-  onMouseLeave() {
-    //this.imgPath = '../../assets/logohd.png';
-  }
-
 }
