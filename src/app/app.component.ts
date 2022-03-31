a1024553f63082ce440445fda920fe3e5d0db3fb
Nikolay Chernobai <nchernobai@abbadox.com>
2022-03-31 21:45:34 +0200

    Fix white line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { SharedService } from './services/shared.service';
 import { SubjectsDataService } from './services/subject-data.service';
 import { TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,13 @@ export class AppComponent implements OnInit, OnDestroy  {
   constructor(public sharedService: SharedService,
               private cdr: ChangeDetectorRef,
               private subjects: SubjectsDataService,
-              private translate: TranslateService) { 
+              private translate: TranslateService,
+              @Inject(DOCUMENT) private document: Document) { 
                 translate.setDefaultLang('ua');
               }
 
   ngOnInit(): void {
+    this.document.body.classList.add('blackBG');
     this.subjects.subject(1)
     .pipe(takeUntil(this.ngUnsubscribe))
     .subscribe(data => {
