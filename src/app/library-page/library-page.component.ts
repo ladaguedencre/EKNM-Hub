a7d45911024816a71b7886580078860cdcb8b0bd
ladaguedencre <valerarezz@gmail.com>
2022-10-08 12:49:35 +0200

    Use mock in library page

--- src/app/library-page/library-page.component.ts
+++ src/app/library-page/library-page.component.ts
@@ -14,7 +14,7 @@ export class LibraryPageComponent implements OnInit {
 
     ngOnInit(): void {
         this.bindingService
-            .getBindings()
+            .getBindingsMock()
             .toPromise()
             .then((bindings) => {
                 if (!bindings) {
